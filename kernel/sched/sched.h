a557cb0178ee90bef5f6200be18b79519b2762c5
Dietmar Eggemann <dietmar.eggemann@arm.com>
2014-11-14 16:08:45 +0000

    sched: Introduce energy data structures
    The struct sched_group_energy represents the per sched_group related
    data which is needed for energy aware scheduling. It contains:
    
      (1) number of elements of the idle state array
      (2) pointer to the idle state array which comprises 'power consumption'
          for each idle state
      (3) number of elements of the capacity state array
      (4) pointer to the capacity state array which comprises 'compute
          capacity and power consumption' tuples for each capacity state
    
    The struct sched_group obtains a pointer to a struct sched_group_energy.
    
    The function pointer sched_domain_energy_f is introduced into struct
    sched_domain_topology_level which will allow the arch to pass a particular
    struct sched_group_energy from the topology shim layer into the scheduler
    core.
    
    The function pointer sched_domain_energy_f has an 'int cpu' parameter
    since the folding of two adjacent sd levels via sd degenerate doesn't work
    for all sd levels. I.e. it is not possible for example to use this feature
    to provide per-cpu energy in sd level DIE on ARM's TC2 platform.
    
    It was discussed that the folding of sd levels approach is preferable
    over the cpu parameter approach, simply because the user (the arch
    specifying the sd topology table) can introduce less errors. But since
    it is not working, the 'int cpu' parameter is the only way out. It's
    possible to use the folding of sd levels approach for
    sched_domain_flags_f and the cpu parameter approach for the
    sched_domain_energy_f at the same time though. With the use of the
    'int cpu' parameter, an extra check function has to be provided to make
    sure that all cpus spanned by a sched group are provisioned with the same
    energy data.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1180,6 +1180,7 @@ struct sched_group {
 	unsigned int		group_weight;
 	struct sched_group_capacity *sgc;
 	int			asym_prefer_cpu;	/* CPU of highest priority in group */
+	const struct sched_group_energy *sge;
 
 	/*
 	 * The CPUs this group covers.
