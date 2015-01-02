18c937bcf4585cecd7c2e007d364ebd3dece259f
Morten Rasmussen <morten.rasmussen@arm.com>
2015-01-02 17:08:52 +0000

    sched: Highest energy aware balancing sched_domain level pointer
    Add another member to the family of per-cpu sched_domain shortcut
    pointers. This one, sd_ea, points to the highest level at which energy
    model is provided. At this level and all levels below all sched_groups
    have energy model data attached.
    
    Partial energy model information is possible but restricted to providing
    energy model data for lower level sched_domains (sd_ea and below) and
    leaving load-balancing on levels above to non-energy-aware
    load-balancing. For example, it is possible to apply energy-aware
    scheduling within each socket on a multi-socket system and let normal
    scheduling handle load-balancing between sockets.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1153,6 +1153,7 @@ DECLARE_PER_CPU(int, sd_llc_id);
 DECLARE_PER_CPU(struct sched_domain_shared *, sd_llc_shared);
 DECLARE_PER_CPU(struct sched_domain *, sd_numa);
 DECLARE_PER_CPU(struct sched_domain *, sd_asym);
+DECLARE_PER_CPU(struct sched_domain *, sd_ea);
 extern struct static_key_false sched_asym_cpucapacity;
 
 struct sched_group_capacity {
--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -408,12 +408,14 @@ DEFINE_PER_CPU(int, sd_llc_id);
 DEFINE_PER_CPU(struct sched_domain_shared *, sd_llc_shared);
 DEFINE_PER_CPU(struct sched_domain *, sd_numa);
 DEFINE_PER_CPU(struct sched_domain *, sd_asym);
+DEFINE_PER_CPU(struct sched_domain *, sd_ea);
 DEFINE_STATIC_KEY_FALSE(sched_asym_cpucapacity);
 
 static void update_top_cache_domain(int cpu)
 {
 	struct sched_domain_shared *sds = NULL;
 	struct sched_domain *sd;
+	struct sched_domain *ea_sd = NULL;
 	int id = cpu;
 	int size = 1;
 
@@ -434,6 +436,14 @@ static void update_top_cache_domain(int cpu)
 
 	sd = highest_flag_domain(cpu, SD_ASYM_PACKING);
 	rcu_assign_pointer(per_cpu(sd_asym, cpu), sd);
+
+	for_each_domain(cpu, sd) {
+		if (sd->groups->sge)
+			ea_sd = sd;
+		else
+			break;
+	}
+	rcu_assign_pointer(per_cpu(sd_ea, cpu), ea_sd);
 }
 
 static void update_asym_cpucapacity(int cpu)
