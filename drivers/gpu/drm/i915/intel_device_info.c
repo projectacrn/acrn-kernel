4b38b246ec1c693204f1142e4ff99653e15771b4
Rodrigo Vivi <rodrigo.vivi@intel.com>
2016-02-12 16:53:02 -0800

    drm/i915/icl: Add initial Icelake definitions.
    Icelake is a Intel® Processor containing Intel® HD Graphics following
    Cannonlake.
    
    This is just an initial Icelake definition. PCI IDs, Icelake support
    and new features comming in following patches.
    
    v2: Add .ddb_size and .has_guc (Michal Wajdeczko).
    v3: Add the ICL_FEATURES macro (Kelvin Gardiner).
    
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/intel_device_info.c
+++ drivers/gpu/drm/i915/intel_device_info.c
@@ -53,6 +53,7 @@ static const char * const platform_names[] = {
 	PLATFORM_NAME(GEMINILAKE),
 	PLATFORM_NAME(COFFEELAKE),
 	PLATFORM_NAME(CANNONLAKE),
+	PLATFORM_NAME(ICELAKE),
 };
 #undef PLATFORM_NAME
 
