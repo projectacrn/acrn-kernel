2e085fc161cdde8b5430d0d5a1002401914d45fd
Tomas Winkler <tomas.winkler@intel.com>
2016-03-29 16:14:01 +0300

    block: export block_class to be used by class interfaces
    Enable access to block devices via class_interface outside
    of the block subsystem.
    
    Change-Id: I6115a9b4655e47ec42e47c9720da8784139557bd
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- block/genhd.c
+++ block/genhd.c
@@ -1287,6 +1287,7 @@ static void disk_release(struct device *dev)
 struct class block_class = {
 	.name		= "block",
 };
+EXPORT_SYMBOL_GPL(block_class);
 
 static char *block_devnode(struct device *dev, umode_t *mode,
 			   kuid_t *uid, kgid_t *gid)
