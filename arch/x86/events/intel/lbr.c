ad54a00ae67a51e5fdadaba4ec094dc1ea98741c
Andi Kleen <ak@linux.intel.com>
2016-08-30 11:15:46 -0700

    x86/perf/lbr: Avoid reading the LBRs when adaptive PEBS handles them
    With adaptive PEBS the CPU can directly supply the LBR information,
    so we don't need to read it again. But the LBRs still need to be
    enabled. Add a special count to the cpuc that distinguishes these
    two cases, and avoid reading the LBRs unnecessarily when PEBS is
    active.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/events/intel/lbr.c
+++ arch/x86/events/intel/lbr.c
@@ -453,6 +453,8 @@ void intel_pmu_lbr_add(struct perf_event *event)
 	 * be 'new'. Conversely, a new event can get installed through the
 	 * context switch path for the first time.
 	 */
+	if (x86_pmu.intel_cap.adaptive_pebs && event->attr.precise_ip > 0)
+		cpuc->lbr_pebs_users++;
 	perf_sched_cb_inc(event->ctx->pmu);
 	if (!cpuc->lbr_users++ && !event->total_time_running)
 		intel_pmu_lbr_reset();
@@ -472,8 +474,11 @@ void intel_pmu_lbr_del(struct perf_event *event)
 		task_ctx->lbr_callstack_users--;
 	}
 
+	if (x86_pmu.intel_cap.adaptive_pebs && event->attr.precise_ip > 0)
+		cpuc->lbr_pebs_users--;
 	cpuc->lbr_users--;
 	WARN_ON_ONCE(cpuc->lbr_users < 0);
+	WARN_ON_ONCE(cpuc->lbr_pebs_users < 0);
 	perf_sched_cb_dec(event->ctx->pmu);
 }
 
@@ -616,7 +621,13 @@ void intel_pmu_lbr_read(void)
 {
 	struct cpu_hw_events *cpuc = this_cpu_ptr(&cpu_hw_events);
 
-	if (!cpuc->lbr_users)
+	/*
+	 * Don't read when all LBRs users are using adaptive PEBS.
+	 *
+	 * This could be smarter and actually check the event,
+	 * but this simple approach seems to work for now.
+	 */
+	if (!cpuc->lbr_users || cpuc->lbr_users == cpuc->lbr_pebs_users)
 		return;
 
 	if (x86_pmu.intel_cap.lbr_format == LBR_FORMAT_32)
