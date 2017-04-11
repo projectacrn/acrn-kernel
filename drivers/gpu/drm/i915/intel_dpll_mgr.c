e27e2d08b81e3391332f773e2f7b35bfa44247d1
Paulo Zanoni <paulo.r.zanoni@intel.com>
2017-04-11 15:37:21 -0300

    drm/i915/icl: compute the combo PHY (DPLL) HDMI registers
    HDMI mode DPLL programming on ICL is the same as CNL, so just reuse
    the CNL code.
    
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>

--- drivers/gpu/drm/i915/intel_dpll_mgr.c
+++ drivers/gpu/drm/i915/intel_dpll_mgr.c
@@ -2203,6 +2203,7 @@ cnl_ddi_calculate_wrpll(int clock,
 			struct skl_wrpll_params *wrpll_params)
 {
 	u32 afe_clock = clock * 5;
+	uint32_t ref_clock;
 	u32 dco_min = 7998000;
 	u32 dco_max = 10000000;
 	u32 dco_mid = (dco_min + dco_max) / 2;
@@ -2235,8 +2236,12 @@ cnl_ddi_calculate_wrpll(int clock,
 
 	cnl_wrpll_get_multipliers(best_div, &pdiv, &qdiv, &kdiv);
 
-	cnl_wrpll_params_populate(wrpll_params, best_dco,
-				  dev_priv->cdclk.hw.ref, pdiv, qdiv, kdiv);
+	ref_clock = dev_priv->cdclk.hw.ref;
+	if (IS_ICELAKE(dev_priv) && ref_clock == 38400)
+		ref_clock = 19200;
+
+	cnl_wrpll_params_populate(wrpll_params, best_dco, ref_clock, pdiv, qdiv,
+				  kdiv);
 
 	return true;
 }
@@ -2387,7 +2392,34 @@ static const struct intel_dpll_mgr cnl_pll_mgr = {
 static bool icl_calc_dpll_state(struct intel_encoder *encoder, int clock,
 				struct intel_dpll_hw_state *pll_hw_state)
 {
-	/* TODO */
+	struct drm_i915_private *dev_priv = to_i915(encoder->base.dev);
+	uint32_t cfgcr0, cfgcr1;
+	struct skl_wrpll_params pll_params = { 0 };
+	bool is_tbt = encoder->port >= PORT_C;
+	bool ret;
+
+	if (is_tbt)
+		ret = false; /* TODO */
+	else if (encoder->type == INTEL_OUTPUT_HDMI)
+		ret = cnl_ddi_calculate_wrpll(clock * 1000, dev_priv,
+					      &pll_params);
+	else
+		ret = false; /* TODO */
+
+	if (!ret)
+		return false;
+
+	cfgcr0 = DPLL_CFGCR0_DCO_FRACTION(pll_params.dco_fraction) |
+		 pll_params.dco_integer;
+
+	cfgcr1 = DPLL_CFGCR1_QDIV_RATIO(pll_params.qdiv_ratio) |
+		 DPLL_CFGCR1_QDIV_MODE(pll_params.qdiv_mode) |
+		 DPLL_CFGCR1_KDIV(pll_params.kdiv) |
+		 DPLL_CFGCR1_PDIV(pll_params.pdiv) |
+		 DPLL_CFGCR1_CENTRAL_FREQ_8400;
+
+	pll_hw_state->cfgcr0 = cfgcr0;
+	pll_hw_state->cfgcr1 = cfgcr1;
 	return true;
 }
 
