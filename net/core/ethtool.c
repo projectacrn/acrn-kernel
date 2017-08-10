01682ffa97db7e325c271e951a5a0a1988088a3f
Ong Boon Leong <boon.leong.ong@intel.com>
2017-08-11 05:43:40 +0800

    net: add Time-Sensitive Networking related netdev features
    Time-Sensitive Networking (TSN) Task Group defines a collection of
    IEEE802.1 specs there are related to improve the timeliness of
    transmitted frame.
    
    We introduce 2 netdev features covered under TSN:-
     a) NETIF_F_HW_EST : IEEE802.1 Qbv Enhancements for Scheduled Traffic.
     b) NETIF_F_HW_FPE : IEEE802.1 Qbu Frame Preemption.
    
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>

--- net/core/ethtool.c
+++ net/core/ethtool.c
@@ -106,6 +106,8 @@ static const char netdev_features_strings[NETDEV_FEATURE_COUNT][ETH_GSTRING_LEN]
 	[NETIF_F_HW_ESP_BIT] =		 "esp-hw-offload",
 	[NETIF_F_HW_ESP_TX_CSUM_BIT] =	 "esp-tx-csum-hw-offload",
 	[NETIF_F_RX_UDP_TUNNEL_PORT_BIT] =	 "rx-udp_tunnel-port-offload",
+	[NETIF_F_HW_EST_BIT] =		 "tx-est",
+	[NETIF_F_HW_FPE_BIT] =		 "tx-fpe",
 };
 
 static const char
