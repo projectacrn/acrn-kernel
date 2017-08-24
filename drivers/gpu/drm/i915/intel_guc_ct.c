b51e324c945e0866e9329e31180cdeed76b43826
Oscar Mateo <oscar.mateo@intel.com>
2017-08-24 10:21:11 -0700

    drm/i915/guc: Tweak CTB for current GuC FW versions
    This patch needs to be removed once the new interface proposed by Michal
    Wajdeczko lands in to the GuC FW.
    
    Suggested-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>

--- drivers/gpu/drm/i915/intel_guc_ct.c
+++ drivers/gpu/drm/i915/intel_guc_ct.c
@@ -576,7 +576,8 @@ static inline unsigned int ct_header_get_action(u32 header)
 
 static inline bool ct_header_is_response(u32 header)
 {
-	return ct_header_get_action(header) == INTEL_GUC_ACTION_DEFAULT;
+#define GUC_CT_MSG_IS_RESPONSE  (1 << 8)
+	return !!(header & GUC_CT_MSG_IS_RESPONSE);
 }
 
 static int ctb_read(struct intel_guc_ct_buffer *ctb, u32 *data)
