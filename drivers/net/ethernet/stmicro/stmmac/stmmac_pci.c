601d159cc033220fa911f5af82fed67e2c32fba8
Ong Boon Leong <boon.leong.ong@intel.com>
2017-09-14 06:01:32 +0800

    REVERTME: net: stmmac: mark EST & FPE are supported for KSL-I
    We mark EST & FPE feature is supported for KSL-I ICP-LP PCH
    that is now being tested in IPv5.00 FPGA image.
    
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_pci.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_pci.c
@@ -236,6 +236,8 @@ static int synp_haps_default_data(struct pci_dev *pdev,
 	plat->pcs_mode = AN_CTRL_PCS_MD_C37_SGMII;
 	plat->force_sf_dma_mode = 0;
 	plat->tso_en = 1;
+	plat->tsn_est_en = 1;
+	plat->tsn_fpe_en = 1;
 
 	plat->rx_queues_to_use = 6;
 	plat->tx_queues_to_use = 4;
