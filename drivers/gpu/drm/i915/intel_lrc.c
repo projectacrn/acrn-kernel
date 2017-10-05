c542ed0acb9f2dffbcc6c64e99dc33f9fdb5bba8
Oscar Mateo <oscar.mateo@intel.com>
2017-10-05 10:21:32 -0700

    drm/i915/icl: Correctly clear lost ctx-switch interrupts across reset for Gen11
    Interrupt handling in Gen11 is quite different from previous platforms.
    
    v2: Rebased (Michel)
    v3: Rebased with wiggle
    v4: Rebased, remove TODO warning correctly (Daniele)
    v5: Rebased, made gen11_gtiir const while at it (Michel)
    v6: Rebased.
    
    Suggested-by: Michel Thierry <michel.thierry@intel.com>
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Michel Thierry <michel.thierry@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>

--- drivers/gpu/drm/i915/intel_lrc.c
+++ drivers/gpu/drm/i915/intel_lrc.c
@@ -787,6 +787,22 @@ execlists_cancel_port_requests(struct intel_engine_execlists * const execlists)
 	execlists_user_end(execlists);
 }
 
+struct gen11_irq_bank_bit {
+	u8 bank;
+	u8 bit;
+};
+
+static const struct gen11_irq_bank_bit gen11_gtiir[] = {
+	[RCS] = {0, GEN11_RCS0},
+	[BCS] = {0, GEN11_BCS},
+	[_VCS(0)] = {1, GEN11_VCS(0)},
+	[_VCS(1)] = {1, GEN11_VCS(1)},
+	[_VCS(2)] = {1, GEN11_VCS(2)},
+	[_VCS(3)] = {1, GEN11_VCS(3)},
+	[_VECS(0)] = {1, GEN11_VECS(0)},
+	[_VECS(1)] = {1, GEN11_VECS(1)},
+};
+
 static void clear_gtiir(struct intel_engine_cs *engine)
 {
 	static const u8 gtiir[] = {
@@ -799,12 +815,6 @@ static void clear_gtiir(struct intel_engine_cs *engine)
 	struct drm_i915_private *dev_priv = engine->i915;
 	int i;
 
-	/* TODO: correctly reset irqs for gen11 */
-	if (WARN_ON_ONCE(INTEL_GEN(engine->i915) >= 11))
-		return;
-
-	GEM_BUG_ON(engine->id >= ARRAY_SIZE(gtiir));
-
 	/*
 	 * Clear any pending interrupt state.
 	 *
@@ -812,13 +822,28 @@ static void clear_gtiir(struct intel_engine_cs *engine)
 	 * double buffered, and so if we only reset it once there may
 	 * still be an interrupt pending.
 	 */
-	for (i = 0; i < 2; i++) {
-		I915_WRITE(GEN8_GT_IIR(gtiir[engine->id]),
+	if (INTEL_GEN(dev_priv) >= 11) {
+		unsigned long irqflags;
+
+		GEM_BUG_ON(engine->id >= ARRAY_SIZE(gen11_gtiir));
+
+		spin_lock_irqsave(&dev_priv->irq_lock, irqflags);
+		gen11_reset_one_iir(dev_priv, gen11_gtiir[engine->id].bank,
+				    gen11_gtiir[engine->id].bit);
+		gen11_reset_one_iir(dev_priv, gen11_gtiir[engine->id].bank,
+				    gen11_gtiir[engine->id].bit);
+		spin_unlock_irqrestore(&dev_priv->irq_lock, irqflags);
+	} else {
+		GEM_BUG_ON(engine->id >= ARRAY_SIZE(gtiir));
+
+		for (i = 0; i < 2; i++) {
+			I915_WRITE(GEN8_GT_IIR(gtiir[engine->id]),
+				   engine->irq_keep_mask);
+			POSTING_READ(GEN8_GT_IIR(gtiir[engine->id]));
+		}
+		GEM_BUG_ON(I915_READ(GEN8_GT_IIR(gtiir[engine->id])) &
 			   engine->irq_keep_mask);
-		POSTING_READ(GEN8_GT_IIR(gtiir[engine->id]));
 	}
-	GEM_BUG_ON(I915_READ(GEN8_GT_IIR(gtiir[engine->id])) &
-		   engine->irq_keep_mask);
 }
 
 static void reset_irq(struct intel_engine_cs *engine)
