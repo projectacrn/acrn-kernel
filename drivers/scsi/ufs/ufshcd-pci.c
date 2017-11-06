e867d623aa0ec54fbd78e11a25cf1138e977ccef
Adrian Hunter <adrian.hunter@intel.com>
2017-11-06 11:01:30 +0200

    scsi: ufshdc-pci: Add yet more Intel PCI Ids
    Add more Intel PCI Ids.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- drivers/scsi/ufs/ufshcd-pci.c
+++ drivers/scsi/ufs/ufshcd-pci.c
@@ -243,6 +243,8 @@ static const struct pci_device_id ufshcd_pci_tbl[] = {
 	{ PCI_VDEVICE(INTEL, 0x34FA), (kernel_ulong_t)&ufs_intel_cnl_hba_vops },
 	{ PCI_VDEVICE(INTEL, 0x34FD), (kernel_ulong_t)&ufs_intel_cnl_hba_vops },
 	{ PCI_VDEVICE(INTEL, 0x38FA), (kernel_ulong_t)&ufs_intel_cnl_hba_vops },
+	{ PCI_VDEVICE(INTEL, 0xA0FA), (kernel_ulong_t)&ufs_intel_cnl_hba_vops },
+	{ PCI_VDEVICE(INTEL, 0xA0FF), (kernel_ulong_t)&ufs_intel_cnl_hba_vops },
 	{ }	/* terminate list */
 };
 
