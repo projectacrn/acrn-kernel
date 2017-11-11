b1ff72803e7ee39de1d00a6a5884804b169fbe72
Kelvin Gardiner <kelvin.gardiner@intel.com>
2017-11-10 16:26:00 -0800

    SIM: drm/i915/icl: Add the ICL PCI ID for simulation
    This patch adds the ICL simulation PCI ID.
    
    V2: Fix the GT3 ID (Paulo)
    V3: Rebase on top of the real IDs (Paulo).
    V4: Add the 11.5 IDs (Kelvin Gardiner).
    V5: Actually add the IDs to i915_drv.c and early-quirks.c (Sreerenj
        Balachandran, Kelvin Gardiner).
    V6: Rebase (Paulo).
    v7: Rebase and move addition to pciidlist[] out to patch with real IDs
        (Lucas).
    v8: Rebase.
    
    Signed-off-by: Kelvin Gardiner <kelvin.gardiner@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- include/drm/i915_pciids.h
+++ include/drm/i915_pciids.h
@@ -446,6 +446,15 @@
 	INTEL_VGA_DEVICE(0x5A4C, info)
 
 /* ICL */
+#define INTEL_ICL_SIM_11_IDS(info) \
+	INTEL_VGA_DEVICE(0xFF05, info)
+
+#define INTEL_ICL_SIM_11_5_IDS(info) \
+	INTEL_VGA_DEVICE(0xFF01, info), \
+	INTEL_VGA_DEVICE(0xFF02, info), \
+	INTEL_VGA_DEVICE(0xFF03, info), \
+	INTEL_VGA_DEVICE(0xFF04, info)
+
 #define INTEL_ICL_11_IDS(info) \
 	INTEL_VGA_DEVICE(0x8A50, info), \
 	INTEL_VGA_DEVICE(0x8A51, info), \
@@ -455,7 +464,8 @@
 	INTEL_VGA_DEVICE(0x8A5A, info), \
 	INTEL_VGA_DEVICE(0x8A5B, info), \
 	INTEL_VGA_DEVICE(0x8A71, info), \
-	INTEL_VGA_DEVICE(0x8A70, info)
+	INTEL_VGA_DEVICE(0x8A70, info), \
+	INTEL_ICL_SIM_11_IDS(info)
 
 #define INTEL_ICL_11_5_IDS(info) \
 	INTEL_VGA_DEVICE(0x8A60, info), \
@@ -466,7 +476,8 @@
 	INTEL_VGA_DEVICE(0x8A40, info), \
 	INTEL_VGA_DEVICE(0x8A45, info), \
 	INTEL_VGA_DEVICE(0x8A43, info), \
-	INTEL_VGA_DEVICE(0x8A44, info)
+	INTEL_VGA_DEVICE(0x8A44, info), \
+	INTEL_ICL_SIM_11_5_IDS(info)
 
 #define INTEL_TGL_12_IDS(info) \
 	INTEL_VGA_DEVICE(0x9A49, info), \
