0ec6b4cfb7ea7b03c1722be8a76b27bfec680de8
Animesh Manna <animesh.manna@intel.com>
2017-11-16 13:02:28 -0200

    drm/i915/icl: Upadte FIA supported lane count for hpd.
    In ICL, Flexible IO Adapter (FIA) muxes data and clocks
    of USB 3.1, thunderbolt and display controller. In DP
    alt mode FIA configure the number of lanes and will be
    used apart from DPCD read to calculate max available
    lanes for DP enablement.
    
    v1: Sent as RFC.
    
    v2: Based on review comments from Jani, Manasi,
    - Register definition modified as per convention. (Jani)
    - Used "tc_port" instead of "port" to avoid any confusion. (Manasi)
    - Used _MASK and _SHIFT macro definition to simplify the lane-info
    calculation.
    
    v3: Based on review cpmments from Paulo,
    - Further simplified the lane info calculation.
    - removed unused macro.
    
    v4: Based on review comments from Paulo,
    - tc_port kept under parenthesis in macro definition.
    - used u32 instread of int while reading register.
    - used MISSING_CASE in default statement of switch case.
    
    v5: Based on review comments from Manasi,
    - changed the function name from icl_get_fia_supported_lane
    to intel_dp_get_fia_supported_lane_cnt.
    - update lane count in intel_dp_max_common_lane_count
    which directly can be used to update max_link_lane_count.
    - wrapper function intel_dp_update_fia_lane_count removed
    as not needed with the above change.
    
    v6 (from Paulo):
    - Rewrite parts of the patch to fit the new series, while keeping the
    same idea.
    
    Signed-off-by: Animesh Manna <animesh.manna@intel.com>
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -10110,4 +10110,9 @@ enum skl_power_gate {
 #define PORT_TX_DFLEXDPCSSS				_MMIO(0x163894)
 #define   DP_PHY_MODE_STATUS_NOT_SAFE(tc_port)		(1 << (tc_port))
 
+#define PORT_TX_DFLEXDPSP			_MMIO(0x1638A0)
+#define   DP_LANE_ASSIGNMENT_SHIFT(tc_port)	((tc_port) * 8)
+#define   DP_LANE_ASSIGNMENT_MASK(tc_port)	(0xf << ((tc_port) * 8))
+#define   DP_LANE_ASSIGNMENT(tc_port, x)	((x) << ((tc_port) * 8))
+
 #endif /* _I915_REG_H_ */
