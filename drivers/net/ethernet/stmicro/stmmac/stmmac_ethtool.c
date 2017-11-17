8bead67639b873818d8e43c09dcc011cb66139e2
Kweh Hock Leong <hock.leong.kweh@intel.com>
2017-11-17 08:38:06 +0800

    net: stmmac: add ethtool hw tunable getter and setter functions for TBS
    Extend ethtool getter and setter tunable to include TBS mode, fetch &
    expiry time offset as well as gsn slot offset.
    
    Signed-off-by: Kweh Hock Leong <hock.leong.kweh@intel.com>
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Reviewed-by: Baoli Zhang <baoli.zhang@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -1155,6 +1155,11 @@ static int stmmac_get_tunable(struct net_device *dev,
 	case ETHTOOL_TX_FPE_AFSZ:
 	case ETHTOOL_TX_FPE_HADV:
 	case ETHTOOL_TX_FPE_RADV:
+	case ETHTOOL_TX_TBS_ESTM:
+	case ETHTOOL_TX_TBS_FTOS:
+	case ETHTOOL_TX_TBS_FGOS:
+	case ETHTOOL_TX_TBS_LEOS:
+	case ETHTOOL_TX_TBS_LEGOS:
 		if (priv->hw->mac->get_tsn_hwtunable)
 			ret = priv->hw->mac->get_tsn_hwtunable(dev,
 				tuna->id, data);
@@ -1186,6 +1191,11 @@ static int stmmac_set_tunable(struct net_device *dev,
 	case ETHTOOL_TX_FPE_AFSZ:
 	case ETHTOOL_TX_FPE_HADV:
 	case ETHTOOL_TX_FPE_RADV:
+	case ETHTOOL_TX_TBS_ESTM:
+	case ETHTOOL_TX_TBS_FTOS:
+	case ETHTOOL_TX_TBS_FGOS:
+	case ETHTOOL_TX_TBS_LEOS:
+	case ETHTOOL_TX_TBS_LEGOS:
 		if (priv->hw->mac->set_tsn_hwtunable)
 			ret = priv->hw->mac->set_tsn_hwtunable(dev,
 				tuna->id, data);
