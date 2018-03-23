d3774e18572b6a4b10fd6d67411b0a5d003b523c
Mathias Nyman <mathias.nyman@linux.intel.com>
2018-03-23 16:42:59 +0200

    usb: define HCD_USB32 speed option for hosts that support USB 3.2 dual-lane
    Hosts that support USB 3.2 Enhaned SuperSpeed can set their hcd speed
    to HCD_USB32 to let usb core and host drivers know that the controller
    supports new USB 3.2 dual-lane features.
    
    make sure usb core handle HCD_USB32 hosts correctly, for now similar
    to HCD_USB32.
    
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>

--- drivers/usb/core/hcd.c
+++ drivers/usb/core/hcd.c
@@ -568,6 +568,7 @@ static int rh_call_control (struct usb_hcd *hcd, struct urb *urb)
 		switch (wValue & 0xff00) {
 		case USB_DT_DEVICE << 8:
 			switch (hcd->speed) {
+			case HCD_USB32:
 			case HCD_USB31:
 				bufp = usb31_rh_dev_descriptor;
 				break;
@@ -592,6 +593,7 @@ static int rh_call_control (struct usb_hcd *hcd, struct urb *urb)
 			break;
 		case USB_DT_CONFIG << 8:
 			switch (hcd->speed) {
+			case HCD_USB32:
 			case HCD_USB31:
 			case HCD_USB3:
 				bufp = ss_rh_config_descriptor;
@@ -2832,6 +2834,7 @@ int usb_add_hcd(struct usb_hcd *hcd,
 	case HCD_USB3:
 		rhdev->speed = USB_SPEED_SUPER;
 		break;
+	case HCD_USB32:
 	case HCD_USB31:
 		rhdev->speed = USB_SPEED_SUPER_PLUS;
 		break;
