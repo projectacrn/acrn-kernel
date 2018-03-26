f54e5630ebb3d4f664ac4e626546ccb483fdc51d
Kumar, Mahesh <mahesh1.kumar@intel.com>
2018-03-26 19:08:06 +0530

    drm/i915/tgl: extend icl ddi_clk_get/select/disable function for tgl
    This patch extends following functions from ICL to TGL
     intel_ddi_clk_select
     intel_ddi_clk_disable
     intel_ddi_dp_voltage_max
     intel_ddi_clock_get
    
    Signed-off-by: Mahesh Kumar <mahesh1.kumar@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/i915/intel_ddi.c
+++ drivers/gpu/drm/i915/intel_ddi.c
@@ -1677,7 +1677,7 @@ static void intel_ddi_clock_get(struct intel_encoder *encoder,
 		bxt_ddi_clock_get(encoder, pipe_config);
 	else if (IS_CANNONLAKE(dev_priv))
 		cnl_ddi_clock_get(encoder, pipe_config);
-	else if (IS_ICELAKE(dev_priv))
+	else if (IS_ICELAKE(dev_priv) || IS_TIGERLAKE(dev_priv))
 		icl_ddi_clock_get(encoder, pipe_config);
 }
 
@@ -2193,7 +2193,7 @@ u8 intel_ddi_dp_voltage_max(struct intel_encoder *encoder)
 	enum port port = encoder->port;
 	int n_entries;
 
-	if (IS_ICELAKE(dev_priv)) {
+	if (IS_ICELAKE(dev_priv) || IS_TIGERLAKE(dev_priv)) {
 		if (intel_is_port_combophy(dev_priv, port))
 			icl_get_combo_buf_trans(dev_priv, port, encoder->type,
 						&n_entries);
@@ -2739,7 +2739,7 @@ static void intel_ddi_clk_select(struct intel_encoder *encoder,
 
 	mutex_lock(&dev_priv->dpll_lock);
 
-	if (IS_ICELAKE(dev_priv)) {
+	if (IS_ICELAKE(dev_priv) || IS_TIGERLAKE(dev_priv)) {
 		if (!intel_is_port_combophy(dev_priv, port))
 			I915_WRITE(DDI_CLK_SEL(port),
 				   icl_pll_to_ddi_pll_sel(encoder, pll));
@@ -2781,7 +2781,7 @@ static void intel_ddi_clk_disable(struct intel_encoder *encoder)
 	struct drm_i915_private *dev_priv = to_i915(encoder->base.dev);
 	enum port port = encoder->port;
 
-	if (IS_ICELAKE(dev_priv)) {
+	if (IS_ICELAKE(dev_priv) || IS_TIGERLAKE(dev_priv)) {
 		if (!intel_is_port_combophy(dev_priv, port))
 			I915_WRITE(DDI_CLK_SEL(port), DDI_CLK_SEL_NONE);
 	} else if (IS_CANNONLAKE(dev_priv)) {
