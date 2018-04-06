149047eb7281eb967dfa2efa5e812fdefff6c3ea
Oscar Mateo <oscar.mateo@intel.com>
2018-04-06 10:11:41 -0700

    drm/i915/icl: Wa_1406680159 and Wa_2201832410
    Disable GWL clock gating to prevent two different issues that
    might cause hangs.
    
    Please notice that one of the issues is pre-production only.
    
    v2: Rebased on top of the WA refactoring
    
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>

--- drivers/gpu/drm/i915/intel_workarounds.c
+++ drivers/gpu/drm/i915/intel_workarounds.c
@@ -751,6 +751,12 @@ static void icl_gt_workarounds_apply(struct drm_i915_private *dev_priv)
 		I915_WRITE(SLICE_UNIT_LEVEL_CLKGATE,
 			   (I915_READ(SLICE_UNIT_LEVEL_CLKGATE) |
 			    MSCUNIT_CLKGATE_DIS));
+
+	/* Wa_1406680159:icl */
+	/* Wa_2201832410:icl (pre-prod, only until C0) */
+	I915_WRITE(SUBSLICE_UNIT_LEVEL_CLKGATE,
+		   (I915_READ(SUBSLICE_UNIT_LEVEL_CLKGATE) |
+		    GWUNIT_CLKGATE_DIS));
 }
 
 void intel_gt_workarounds_apply(struct drm_i915_private *dev_priv)
