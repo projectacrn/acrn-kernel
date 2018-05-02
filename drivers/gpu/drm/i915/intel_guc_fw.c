f05a8fd30756d10b73e85f8c6c7738a56ac59407
Oscar Mateo <oscar.mateo@intel.com>
2018-05-02 19:48:29 +0000

    drm/i915/icl/guc: Define the GuC firmware version for Icelake
    A GuC firmware for Icelake is now available. Let's use it.
    
    v2: Split out the Cannonlake stuff in a separate patch (Michal)
    
    v3: Rebased
    
    v4:
      - Rebased
      - Split out MODULE_FIRMWARE so we don't accidentally push it
        before linux-firmware (Joonas)
    
    v5: Use the latest firmware (v23.120)
    v6: Use the latest firmware (v26.171)
    v7: Rebased (remove guc-core-family)
    v8: Use the latest firmware (v27.182)
    v9: Use the latest firmware (v27.185)
    
    Signed-off-by: Michel Thierry <michel.thierry@intel.com>
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: John Spotswood <john.a.spotswood@intel.com>
    Cc: Tony Ye <tony.ye@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Anusha Srivatsa <anusha.srivatsa@intel.com>

--- drivers/gpu/drm/i915/intel_guc_fw.c
+++ drivers/gpu/drm/i915/intel_guc_fw.c
@@ -39,6 +39,9 @@
 #define KBL_FW_MAJOR 9
 #define KBL_FW_MINOR 39
 
+#define ICL_FW_MAJOR 27
+#define ICL_FW_MINOR 185
+
 #define GUC_FW_PATH(platform, major, minor) \
        "i915/" __stringify(platform) "_guc_ver" __stringify(major) "_" __stringify(minor) ".bin"
 
@@ -50,6 +53,8 @@ MODULE_FIRMWARE(I915_BXT_GUC_UCODE);
 
 #define I915_KBL_GUC_UCODE GUC_FW_PATH(kbl, KBL_FW_MAJOR, KBL_FW_MINOR)
 MODULE_FIRMWARE(I915_KBL_GUC_UCODE);
+
+#define I915_ICL_GUC_UCODE GUC_FW_PATH(icl, ICL_FW_MAJOR, ICL_FW_MINOR)
 
 static void guc_fw_select(struct intel_uc_fw *guc_fw)
 {
@@ -77,6 +82,10 @@ static void guc_fw_select(struct intel_uc_fw *guc_fw)
 		guc_fw->path = I915_KBL_GUC_UCODE;
 		guc_fw->major_ver_wanted = KBL_FW_MAJOR;
 		guc_fw->minor_ver_wanted = KBL_FW_MINOR;
+	} else if (IS_ICELAKE(dev_priv)) {
+		guc_fw->path = I915_ICL_GUC_UCODE;
+		guc_fw->major_ver_wanted = ICL_FW_MAJOR;
+		guc_fw->minor_ver_wanted = ICL_FW_MINOR;
 	} else {
 		DRM_WARN("%s: No firmware known for this platform!\n",
 			 intel_uc_fw_type_repr(guc_fw->type));
