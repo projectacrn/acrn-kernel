7ff48d1b9eb0871e3d3ff3a7c2fb9c88a87d39fb
Oscar Mateo <oscar.mateo@intel.com>
2018-05-02 19:48:32 +0000

    drm/i915/icl/guc: Update GuC parameter initialization for Icelake
    Some minor init parameter changes in the GuC firmware for ICL.
    
    The ADS ("Additional Data Struct") has been moved to its own dword
    to free up more control bits in the GUC_CTL_DEBUG param (since this
    has grown in Gen11 to the point where the ADS address doesn't fit).
    
    Also, some flags in GUC_CTL_FEATURE have disappeared because they
    are not used anymore (we don't have to specify that we are doing
    kernel submissions because the difference between kernel and direct
    submissions is not so big now, and there is now space in the ADS
    for the VCS and VECS fuse masks).
    
    v2: Rebased
    v3: Rebased
    v4: Improve GUC_CTL_FEATURE initialization logic (Joonas)
    v5: Don't break guc submission in pre gen11
    v6: Update GuC feature flags to latest values (Michel)
    v7:
      - Split the feature flags for more clarity (Joonas)
      - Remove the Gen11 feature flags that we don't use anyway.
      - Better commit message.
    v8: Rebased (s/i915_modparams.enable_guc_submission/USES_GUC_SUBMISSION(dev_priv))
    v9: Rebased.
    v10: Rebased.
    v11: Rebased on top of early HuC enablement (Oscar)
    
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Michel Thierry <michel.thierry@intel.com>
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>

--- drivers/gpu/drm/i915/intel_guc.c
+++ drivers/gpu/drm/i915/intel_guc.c
@@ -225,6 +225,24 @@ static u32 get_log_control_flags(void)
 	return flags;
 }
 
+static u32 guc_feature_flags(struct drm_i915_private *dev_priv)
+{
+	u32 feature_flags = 0;
+
+	if (INTEL_GEN(dev_priv) >= 11) {
+		if (!USES_GUC_SUBMISSION(dev_priv))
+			feature_flags |= GEN11_GUC_CTL_DISABLE_SCHEDULER;
+	} else {
+		feature_flags |= GUC_CTL_VCS2_ENABLED;
+		if (USES_GUC_SUBMISSION(dev_priv))
+			feature_flags |= GUC_CTL_KERNEL_SUBMISSIONS;
+		else
+			feature_flags |= GUC_CTL_DISABLE_SCHEDULER;
+	}
+
+	return feature_flags;
+}
+
 /*
  * Initialise the GuC parameter block before starting the firmware
  * transfer. These parameters are read by the firmware on startup
@@ -248,37 +266,28 @@ void intel_guc_init_params(struct intel_guc *guc)
 
 	params[GUC_CTL_WA] |= GUC_CTL_WA_UK_BY_DRIVER;
 
-	if (INTEL_GEN(dev_priv) >= 11) {
-		params[GUC_CTL_FEATURE] |= GEN11_GUC_CTL_DISABLE_SCHEDULER;
-	} else {
-		params[GUC_CTL_FEATURE] |= GUC_CTL_DISABLE_SCHEDULER;
-		params[GUC_CTL_FEATURE] |= GUC_CTL_VCS2_ENABLED;
-	}
+	params[GUC_CTL_FEATURE] |= guc_feature_flags(dev_priv);
 
 	params[GUC_CTL_LOG_PARAMS] = guc->log.flags;
 
 	params[GUC_CTL_DEBUG] = get_log_control_flags();
 
-	/* If GuC submission is enabled, set up additional parameters here */
 	if (USES_GUC_SUBMISSION(dev_priv)) {
 		u32 ads = intel_guc_ggtt_offset(guc,
 						guc->ads_vma) >> PAGE_SHIFT;
-		u32 pgs = intel_guc_ggtt_offset(guc, guc->stage_desc_pool);
+		u32 pgs = intel_guc_ggtt_offset(guc, guc->stage_desc_pool) >> PAGE_SHIFT;
 		u32 ctx_in_16 = guc->max_stage_desc / 16;
 
-		GEM_BUG_ON(INTEL_GEN(dev_priv) >= 11);
+		if (INTEL_GEN(dev_priv) >= 11) {
+			params[GEN11_GUC_ADS_CTL] = GEN11_GUC_ADS_ENABLE;
+			params[GEN11_GUC_ADS_CTL] |= ads << GEN11_GUC_ADS_ADDR_SHIFT;
+		} else {
+			params[GUC_CTL_DEBUG] |= GUC_ADS_ENABLED;
+			params[GUC_CTL_DEBUG] |= ads << GUC_ADS_ADDR_SHIFT;
+		}
 
-		params[GUC_CTL_DEBUG] |= ads << GUC_ADS_ADDR_SHIFT;
-		params[GUC_CTL_DEBUG] |= GUC_ADS_ENABLED;
-
-		pgs >>= PAGE_SHIFT;
 		params[GUC_CTL_CTXINFO] = (pgs << GUC_CTL_BASE_ADDR_SHIFT) |
 			(ctx_in_16 << GUC_CTL_CTXNUM_IN16_SHIFT);
-
-		params[GUC_CTL_FEATURE] |= GUC_CTL_KERNEL_SUBMISSIONS;
-
-		/* Unmask this bit to enable the GuC's internal scheduler */
-		params[GUC_CTL_FEATURE] &= ~GUC_CTL_DISABLE_SCHEDULER;
 	}
 
 	/*
