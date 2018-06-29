88bb2f6ae1f6cbbecaee50c503d29ddc8b6b7152
Fei Jiang <fei.jiang@intel.com>
2018-06-29 09:51:05 +0800

    drm/i915/gvt: handle plane wm register update for PVMMIO_PLANE_WM_UPDATE pvmmio
    When pvmmio level PVMMIO_PLANE_WM_UPDATE is enabled, need handle multiple plane
    wm related registers updating together.
    sos only patch.
    
    Signed-off-by: Fei Jiang <fei.jiang@intel.com>

--- drivers/gpu/drm/i915/gvt/handlers.c
+++ drivers/gpu/drm/i915/gvt/handlers.c
@@ -871,6 +871,27 @@ static int skl_plane_surf_write(struct intel_vgpu *vgpu, unsigned int offset,
 	return 0;
 }
 
+static int pv_plane_wm_mmio_write(struct intel_vgpu *vgpu, unsigned int offset,
+		void *p_data, unsigned int bytes)
+{
+	unsigned int pipe = SKL_PLANE_REG_TO_PIPE(offset);
+	unsigned int plane = SKL_PLANE_REG_TO_PLANE(offset);
+	struct pv_plane_wm_update *pv_plane_wm =
+		&vgpu->mmio.shared_page->pv_plane_wm;
+	int level;
+	if (VGPU_PVMMIO(vgpu) & PVMMIO_PLANE_WM_UPATE) {
+		for (level = 0; level <= pv_plane_wm->max_wm_level; level++)
+			skl_plane_mmio_write(vgpu,
+			    i915_mmio_reg_offset(PLANE_WM(pipe, plane, level)),
+			    &pv_plane_wm->plane_wm_level[level], 4);
+		skl_plane_mmio_write(vgpu,
+			i915_mmio_reg_offset(PLANE_WM_TRANS(pipe, plane)),
+			&pv_plane_wm->plane_trans_wm_level, 4);
+		/* null function for PLANE_BUF_CFG and PLANE_NV12_BUF_CFG */
+	}
+	return 0;
+}
+
 static int trigger_aux_channel_interrupt(struct intel_vgpu *vgpu,
 		unsigned int reg)
 {
@@ -3040,7 +3061,7 @@ static int init_skl_mmio_info(struct intel_gvt *gvt)
 
 	MMIO_PLANES_SDH(_PLANE_WM_BASE, 4 * 8, D_SKL_PLUS, NULL, NULL);
 	MMIO_PLANES_DH(PLANE_WM_TRANS, D_SKL_PLUS, NULL, NULL);
-	MMIO_PLANES_DH(PLANE_NV12_BUF_CFG, D_SKL_PLUS, NULL, NULL);
+	MMIO_PLANES_DH(PLANE_NV12_BUF_CFG, D_SKL_PLUS, NULL, pv_plane_wm_mmio_write);
 	MMIO_PLANES_DH(PLANE_BUF_CFG, D_SKL_PLUS, NULL, NULL);
 
 	MMIO_D(0x8f074, D_SKL | D_KBL);
@@ -3203,7 +3224,7 @@ static int init_bxt_mmio_info(struct intel_gvt *gvt)
 		MMIO_PLANES_DH(PLANE_WM_TRANS, D_BXT, NULL, skl_plane_mmio_write);
 	}
 
-	MMIO_PLANES_DH(PLANE_NV12_BUF_CFG, D_BXT, NULL, NULL);
+	MMIO_PLANES_DH(PLANE_NV12_BUF_CFG, D_BXT, NULL, pv_plane_wm_mmio_write);
 	MMIO_PLANES_DH(PLANE_BUF_CFG, D_BXT, NULL, NULL);
 
 	MMIO_F(0x80000, 0x3000, 0, 0, 0, D_BXT, NULL, NULL);
