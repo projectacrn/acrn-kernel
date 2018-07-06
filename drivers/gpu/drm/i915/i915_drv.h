bf2e852795250574d24cf5985dcd6ff64b8bb22a
Oscar Mateo <oscar.mateo@intel.com>
2018-07-05 22:43:41 -0700

    drm/i915/icl: Some workarounds have been fixed on Icelake 11.5
    v2: Rebased
    v3: Rebased after the WA refactoring
    v4: Rebased after icl WA landed in drm-tip
    v5: Replace IS_ICL_11_5 macro to use has_global_mocs rather than
        has_resource_streamer since no Icelake platform has RS (Lucas).
    
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2432,6 +2432,8 @@ intel_info(const struct drm_i915_private *dev_priv)
 				 (dev_priv)->info.gt == 3)
 #define IS_CNL_WITH_PORT_F(dev_priv)   (IS_CANNONLAKE(dev_priv) && \
 					(INTEL_DEVID(dev_priv) & 0x0004) == 0x0004)
+#define IS_ICL_11_5(dev_priv)  (IS_ICELAKE(dev_priv) && \
+                                HAS_GLOBAL_MOCS_REGISTERS(dev_priv))
 
 #define IS_ALPHA_SUPPORT(intel_info) ((intel_info)->is_alpha_support)
 
