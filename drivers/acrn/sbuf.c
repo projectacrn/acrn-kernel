teslamotors/linux

Shared_buf: added hypercall for shared_buf setup
Change-Id: I24ad2f767c7d633ad41d787c7d1a052b0fb75fb4
Tracked-On: https://rtc.intel.com/ccm0001001/resource/itemName/com.ibm.team.workitem.WorkItem/216912
Signed-off-by: Li, Fei1 <fei1.li@intel.com>

drivers/cwp/sbuf.c
@@ -57,6 +57,8 @@
 
 #include <linux/gfp.h>
 #include <asm/pgtable.h>
+#include <linux/vhm/cwp_hv_defs.h>
+#include <linux/vhm/vhm_hypercall.h>
 #include "sbuf.h"
 
 static inline bool sbuf_is_empty(shared_buf_t *sbuf)
@@ -163,6 +165,25 @@ int sbuf_get(shared_buf_t *sbuf, uint8_t *data)
 	return sbuf->ele_size;
 }
 EXPORT_SYMBOL(sbuf_get);
+
+int sbuf_share_setup(uint32_t pcpu_id, uint32_t sbuf_id, shared_buf_t *sbuf)
+{
+	struct sbuf_setup_param ssp;
+
+	ssp.pcpu_id = pcpu_id;
+	ssp.sbuf_id = sbuf_id;
+
+	if (!sbuf) {
+		ssp.gpa = 0;
+	} else {
+		BUG_ON(!virt_addr_valid(sbuf));
+		ssp.gpa = virt_to_phys(sbuf);
+	}
+	pr_info("setup phys add = 0x%llx\n", ssp.gpa);
+
+	return hcall_setup_sbuf(virt_to_phys(&ssp));
+}
+EXPORT_SYMBOL(sbuf_share_setup);
 
 shared_buf_t *sbuf_construct(uint32_t ele_num, uint32_t ele_size,
 				uint64_t paddr)

drivers/vhm/vhm_hypercall.c
@@ -77,6 +77,11 @@ inline long hcall_destroy_vm(unsigned long vmid)
 	return  cwp_hypercall1(HC_DESTROY_VM, vmid);
 }
 
+inline long hcall_setup_sbuf(unsigned long sbuf_head)
+{
+	return cwp_hypercall1(HC_SETUP_SBUF, sbuf_head);
+}
+
 inline long hcall_set_memmap(unsigned long vmid, unsigned long memmap)
 {
 	return cwp_hypercall2(HC_VM_SET_MEMMAP, vmid, memmap);

projectacrn/acrn-kernel

drivers/acrn/sbuf.c
@@ -57,6 +57,8 @@
 
 #include <linux/gfp.h>
 #include <asm/pgtable.h>
+#include <linux/vhm/acrn_hv_defs.h>
+#include <linux/vhm/vhm_hypercall.h>
 #include "sbuf.h"
 
 static inline bool sbuf_is_empty(shared_buf_t *sbuf)
@@ -163,6 +165,25 @@ int sbuf_get(shared_buf_t *sbuf, uint8_t *data)
 	return sbuf->ele_size;
 }
 EXPORT_SYMBOL(sbuf_get);
+
+int sbuf_share_setup(uint32_t pcpu_id, uint32_t sbuf_id, shared_buf_t *sbuf)
+{
+	struct sbuf_setup_param ssp;
+
+	ssp.pcpu_id = pcpu_id;
+	ssp.sbuf_id = sbuf_id;
+
+	if (!sbuf) {
+		ssp.gpa = 0;
+	} else {
+		BUG_ON(!virt_addr_valid(sbuf));
+		ssp.gpa = virt_to_phys(sbuf);
+	}
+	pr_info("setup phys add = 0x%llx\n", ssp.gpa);
+
+	return hcall_setup_sbuf(virt_to_phys(&ssp));
+}
+EXPORT_SYMBOL(sbuf_share_setup);
 
 shared_buf_t *sbuf_construct(uint32_t ele_num, uint32_t ele_size,
 				uint64_t paddr)

drivers/vhm/vhm_hypercall.c
@@ -77,6 +77,11 @@ inline long hcall_destroy_vm(unsigned long vmid)
 	return  acrn_hypercall1(HC_DESTROY_VM, vmid);
 }
 
+inline long hcall_setup_sbuf(unsigned long sbuf_head)
+{
+	return acrn_hypercall1(HC_SETUP_SBUF, sbuf_head);
+}
+
 inline long hcall_set_memmap(unsigned long vmid, unsigned long memmap)
 {
 	return acrn_hypercall2(HC_VM_SET_MEMMAP, vmid, memmap);