teslamotors/linux

VHM: add API to do guest gpa2hpa translation
Added API vhm_vm_gpa2hpa: do translation between gpa and hpa for
corresponding guest.

Change-Id: I5ccdc3c6ac73d02d854878957093895c7f0cbee6
Tracked-On: 218445
Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
Reviewed-on: https://android.intel.com/614850
Reviewed-by: Chi, Mingqiang <mingqiang.chi@intel.com>
Reviewed-by: Dong, Eddie <eddie.dong@intel.com>
Tested-by: Dong, Eddie <eddie.dong@intel.com>

drivers/vhm/vhm_hypercall.c
@@ -73,6 +73,11 @@ inline long hcall_set_memmap(unsigned long vmid, unsigned long memmap)
 	return cwp_hypercall2(HC_VM_SET_MEMMAP, vmid, memmap);
 }
 
+inline long hcall_vm_gpa2hpa(unsigned long vmid, unsigned long gpa2hpa)
+{
+	return cwp_hypercall2(HC_VM_GPA2HPA, vmid, gpa2hpa);
+}
+
 inline long vhm_create_vm(struct vhm_vm *vm, unsigned long ioctl_param)
 {
 	long ret = 0;

projectacrn/acrn-kernel

drivers/vhm/vhm_hypercall.c
@@ -73,6 +73,11 @@ inline long hcall_set_memmap(unsigned long vmid, unsigned long memmap)
 	return acrn_hypercall2(HC_VM_SET_MEMMAP, vmid, memmap);
 }
 
+inline long hcall_vm_gpa2hpa(unsigned long vmid, unsigned long gpa2hpa)
+{
+	return acrn_hypercall2(HC_VM_GPA2HPA, vmid, gpa2hpa);
+}
+
 inline long vhm_create_vm(struct vhm_vm *vm, unsigned long ioctl_param)
 {
 	long ret = 0;