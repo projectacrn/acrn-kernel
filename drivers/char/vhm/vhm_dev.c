teslamotors/linux

vcpu: export vcpu create interface to DM
Change-Id: I64a179c2452f67285f347bbaa60c702dec5f55de
Tracked-On: 218445
Signed-off-by: Yin Fengwei <fengwei.yin@intel.com>
Reviewed-on: https://android.intel.com/614858
Reviewed-by: Chi, Mingqiang <mingqiang.chi@intel.com>
Reviewed-by: Dong, Eddie <eddie.dong@intel.com>
Tested-by: Dong, Eddie <eddie.dong@intel.com>

drivers/char/vhm/vhm_dev.c
@@ -224,6 +224,23 @@ static long vhm_dev_ioctl(struct file *filep,
 		return ret;
 	}
 
+	case IC_CREATE_VCPU: {
+		struct cwp_create_vcpu cv;
+
+		if (copy_from_user(&cv, (void *)ioctl_param,
+				sizeof(struct cwp_create_vcpu)))
+			return -EFAULT;
+
+		ret = cwp_hypercall2(HC_CREATE_VCPU, vm->vmid,
+				virt_to_phys(&cv));
+		if (ret < 0) {
+			pr_err("vhm: failed to create vcpu %ld!\n", cv.vcpuid);
+			return -EFAULT;
+		}
+
+		return ret;
+	}
+
 	case IC_ALLOC_MEMSEG: {
 		struct vm_memseg memseg;
 

projectacrn/acrn-kernel

drivers/char/vhm/vhm_dev.c
@@ -224,6 +224,23 @@ static long vhm_dev_ioctl(struct file *filep,
 		return ret;
 	}
 
+	case IC_CREATE_VCPU: {
+		struct acrn_create_vcpu cv;
+
+		if (copy_from_user(&cv, (void *)ioctl_param,
+				sizeof(struct acrn_create_vcpu)))
+			return -EFAULT;
+
+		ret = acrn_hypercall2(HC_CREATE_VCPU, vm->vmid,
+				virt_to_phys(&cv));
+		if (ret < 0) {
+			pr_err("vhm: failed to create vcpu %ld!\n", cv.vcpuid);
+			return -EFAULT;
+		}
+
+		return ret;
+	}
+
 	case IC_ALLOC_MEMSEG: {
 		struct vm_memseg memseg;
 