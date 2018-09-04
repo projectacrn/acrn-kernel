rantogno/linux

NOT_UPSTREAM: drm/i915/guc/tgl: GuC semaphore feature flags
To handle staged additions to the models from HW/fulsim teams the GuC FW
gates all new HW features behind feature flags.

This patch add the feature flags for the new token-based semaphore
mechanism. We need to enable the new mechanism as the CSB changes are
part of it. Those changes landed in HW/fulsim many months ago so there
should be no risk of anyone running on models that don't have the support.

The flags will be removed from GuC FW after RTL freeze and this patch
can be dropped at that point.

Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
Signed-off-by: Michel Thierry <michel.thierry@intel.com>
Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

drivers/gpu/drm/i915/intel_guc.c
@@ -279,6 +279,13 @@ static u32 guc_feature_flags(struct drm_i915_private *dev_priv)
 			feature_flags |= GUC_CTL_DISABLE_SCHEDULER;
 	}
 
+	/* temp flags required by fw to enable WIP hw features */
+#define FTR_ENABLE_HW_SEMAPHORE_PHASE_1 BIT(8)
+#define FTR_ENABLE_HW_SEMAPHORE_PHASE_2 BIT(9)
+	if (INTEL_GEN(dev_priv) >= 12)
+		feature_flags |= FTR_ENABLE_HW_SEMAPHORE_PHASE_1 |
+				 FTR_ENABLE_HW_SEMAPHORE_PHASE_2;
+
 	return feature_flags;
 }
 

projectacrn/acrn-kernel

drivers/gpu/drm/i915/intel_guc.c
@@ -296,6 +296,13 @@ static u32 guc_ctl_feature_flags(struct intel_guc *guc)
 			flags |= GUC_CTL_DISABLE_SCHEDULER;
 	}
 
+	/* temp flags required by fw to enable WIP hw features */
+#define FTR_ENABLE_HW_SEMAPHORE_PHASE_1 BIT(8)
+#define FTR_ENABLE_HW_SEMAPHORE_PHASE_2 BIT(9)
+	if (INTEL_GEN(guc_to_i915(guc)) >= 12)
+		flags |= FTR_ENABLE_HW_SEMAPHORE_PHASE_1 |
+			FTR_ENABLE_HW_SEMAPHORE_PHASE_2;
+
 	return flags;
 }
 