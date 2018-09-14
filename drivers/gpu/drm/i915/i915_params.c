4ddfbb89987ed8f23651e1b65eb676b45e6fd814
Pei Zhang <pei.zhang@intel.com>
2018-09-14 16:10:19 +0800

    drm/i915/gvt: add module parameter enable_pvmmio
    This uint type module parameter is used to control the pvmmio
    features for MMIO emulation in GVT. This parameter is default 0.
    Its permission type is 0400 which means user could only change its
    value through the cmdline, this is to prevent the dynamic modification
    during runtime which would break the pvmmio internal logic.
    
    Notice: this patch is required to be applied to guest kernel.
    
    Change-Id: I570f1fe02101e518595c02fce67601b692871aa9
    Signed-off-by: Pei Zhang <pei.zhang@intel.com>
    Signed-off-by: Jiang, Fei <fei.jiang@intel.com>
    Acknowledged-by: Singh, Satyeshwar <satyeshwar.singh@intel.com>
    Reviewed-on:
    Reviewed-by: He, Min <min.he@intel.com>
    Reviewed-by: Jiang, Fei <fei.jiang@intel.com>
    Reviewed-by: Dong, Eddie <eddie.dong@intel.com>
    Tested-by: Dong, Eddie <eddie.dong@intel.com>

--- drivers/gpu/drm/i915/i915_params.c
+++ drivers/gpu/drm/i915/i915_params.c
@@ -199,6 +199,11 @@ i915_param_named(enable_gvt, bool, 0400,
 i915_param_named(domain_scaler_owner, int, 0400,
         "scaler owners for each domain and for each pipe ids can be from 0-F");
 
+
+i915_param_named(enable_pvmmio, uint, 0400,
+	"Enable pv mmio feature and set pvmmio level, default 1."
+	"This parameter could only set from host, guest value is set through vgt_if");
+
 static __always_inline void _print_param(struct drm_printer *p,
 					 const char *name,
 					 const char *type,
