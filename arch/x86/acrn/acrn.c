fd093cca64b3d86761437fe53c9a007004556914
Zhao Yakui <yakui.zhao@intel.com>
2018-10-16 13:38:18 +0800

    Kernel/Acrn: Use HYPERVISOR_CALLBACK_VECTOR for Acrn upcall vector
    Linux kernel uses the HYPERVISOR_CALLBACK_VECTOR for hypervisor upcall vector.
    And it is already used for Xen and HyperV.
    After Acrn hypervisor is detected, it will also use this defined vector as notify
    vector to kernel.
    
    Other minor fix: The X2APIC is exported by checking boot_cpu_has(X86_FEATURE_X2APIC)
    The x86_hyper_acrn is changed to __initconst.
    The EXPORT_SYMBOL is removed for x86_hyper_acrn
    
    Tracked-on: https://github.com/projectacrn/acrn-hypervisor/issues/1325
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>

--- arch/x86/acrn/acrn.c
+++ arch/x86/acrn/acrn.c
@@ -33,6 +33,12 @@
  *
  */
 #include <asm/hypervisor.h>
+#include <asm/acrnhyper.h>
+#include <asm/irq_vectors.h>
+#include <asm/irq_regs.h>
+#include <asm/desc.h>
+#include <asm/apic.h>
+#include <asm/cpufeature.h>
 
 static uint32_t __init acrn_detect(void)
 {
@@ -41,6 +47,7 @@ static uint32_t __init acrn_detect(void)
 
 static void __init acrn_init_platform(void)
 {
+	alloc_intr_gate(HYPERVISOR_CALLBACK_VECTOR, acrn_hv_callback_vector);
 }
 
 static void acrn_pin_vcpu(int cpu)
@@ -50,8 +57,7 @@ static void acrn_pin_vcpu(int cpu)
 
 static bool acrn_x2apic_available(void)
 {
-	/* do not support x2apic */
-	return false;
+	return boot_cpu_has(X86_FEATURE_X2APIC);
 }
 
 static void __init acrn_init_mem_mapping(void)
@@ -59,7 +65,38 @@ static void __init acrn_init_mem_mapping(void)
 	/* do nothing here now */
 }
 
-const struct hypervisor_x86 x86_hyper_acrn = {
+
+static void (*acrn_intr_handler)(void);
+/*
+ * Handler for ACRN_HV_CALLBACK.
+ */
+__visible void acrn_hv_vector_handler(struct pt_regs *regs)
+{
+	struct pt_regs *old_regs = set_irq_regs(regs);
+
+	entering_ack_irq();
+#ifdef CONFIG_X86
+	inc_irq_stat(irq_hv_callback_count);
+#endif
+
+	if (acrn_intr_handler)
+		acrn_intr_handler();
+
+	exiting_irq();
+	set_irq_regs(old_regs);
+}
+
+void acrn_setup_intr_irq(void (*handler)(void))
+{
+	acrn_intr_handler = handler;
+}
+
+void acrn_remove_intr_irq(void)
+{
+	acrn_intr_handler = NULL;
+}
+
+const __initconst struct hypervisor_x86 x86_hyper_acrn = {
 	.name                   = "ACRN",
 	.detect                 = acrn_detect,
 	.type                 	= X86_HYPER_ACRN,
@@ -68,4 +105,5 @@ const struct hypervisor_x86 x86_hyper_acrn = {
 	.init.x2apic_available  = acrn_x2apic_available,
 	.init.init_mem_mapping	= acrn_init_mem_mapping,
 };
-EXPORT_SYMBOL(x86_hyper_acrn);
+EXPORT_SYMBOL(acrn_setup_intr_irq);
+EXPORT_SYMBOL(acrn_remove_intr_irq);
