7487006b2a646a6ab446986914dfb08e781ba5d6
Reinette Chatre <reinette.chatre@intel.com>
2019-04-25 13:52:42 -0700

    x86/CPU: Expose if cache is inclusive of lower level caches
    Deterministic cache parameters can be learned from CPUID leaf 04H.
    Executing CPUID with a particular index in EAX would return the cache
    parameters associated with that index in the EAX, EBX, ECX, and EDX
    registers.
    
    At this time, when discovering cache parameters for a particular cache
    index, only the parameters returned in EAX, EBX, and ECX are parsed.
    Parameters returned in EDX are ignored. One of the parameters in EDX,
    whether the cache is inclusive of lower level caches, is valuable to
    know when determining if a system can support L3 cache pseudo-locking.
    If the L3 cache is not inclusive then pseudo-locked data within the L3
    cache would be evicted when migrated to L2.
    
    Add support for parsing the cache parameters obtained from EDX and make
    the inclusive cache parameter available via the cacheinfo that can be
    queried from the cache pseudo-locking code.
    
    Do not expose this information to user space at this time. At this time
    this information is required within the kernel only. Also, it is
    not obvious what the best formatting of this information should be in
    support of the variety of ways users may use this information.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- include/linux/cacheinfo.h
+++ include/linux/cacheinfo.h
@@ -33,6 +33,8 @@ extern unsigned int coherency_max_size;
  * @physical_line_partition: number of physical cache lines sharing the
  *	same cachetag
  * @size: Total size of the cache
+ * @inclusive: Cache is inclusive of lower level caches. Only valid if
+ *	CACHE_INCLUSIVE_SET attribute is set.
  * @shared_cpu_map: logical cpumask representing all the cpus sharing
  *	this cache node
  * @attributes: bitfield representing various cache attributes
@@ -55,6 +57,7 @@ struct cacheinfo {
 	unsigned int ways_of_associativity;
 	unsigned int physical_line_partition;
 	unsigned int size;
+	unsigned int inclusive;
 	cpumask_t shared_cpu_map;
 	unsigned int attributes;
 #define CACHE_WRITE_THROUGH	BIT(0)
@@ -66,6 +69,7 @@ struct cacheinfo {
 #define CACHE_ALLOCATE_POLICY_MASK	\
 	(CACHE_READ_ALLOCATE | CACHE_WRITE_ALLOCATE)
 #define CACHE_ID		BIT(4)
+#define CACHE_INCLUSIVE_SET	BIT(5)
 	void *fw_token;
 	bool disable_sysfs;
 	void *priv;
