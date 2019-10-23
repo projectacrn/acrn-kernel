a84948e993148b44c39f94c112382ac857312b32
Junming Liu <junming.liu@intel.com>
2019-10-23 18:35:02 +0000

    drm/i915/gvt: Add a module parameter to disable GPU frequency adjustment for VGPU workload
    Add a module parameter to disable GPU frequency adjustment for VGPU workload, by default this parameter is false.
    
    Tracked-On: projectacrn/acrn-hypervisor#3918
    
    Signed-off-by: Junming Liu <junming.liu@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>

--- drivers/gpu/drm/i915/gvt/scheduler.c
+++ drivers/gpu/drm/i915/gvt/scheduler.c
@@ -277,7 +277,8 @@ static int shadow_context_status_change(struct notifier_block *nb,
 
 	switch (action) {
 	case INTEL_CONTEXT_SCHEDULE_IN:
-		schedule_work(&gvt->active_hp_work);
+		if(!i915_modparams.enable_hp_work)
+			schedule_work(&gvt->active_hp_work);
 		if(!i915_modparams.enable_context_restore) {
 			atomic_set(&workload->shadow_ctx_active, 1);
 			break;
