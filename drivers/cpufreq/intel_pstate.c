intel/mainline-tracking

cpufreq: intel_pstate: Add Elkhart lake support to intel_pstate
Add Atom based EHL support to intel_pstate.

Cc: David E Box <david.e.box@intel.com>
Cc: Rajneesh Bharadwaj <rajneesh.bhardwaj@intel.com>
Signed-off-by: Gayatri Kammela <gayatri.kammela@intel.com>

drivers/cpufreq/intel_pstate.c
@@ -1887,6 +1887,7 @@ static const struct x86_cpu_id intel_pstate_cpu_ids[] = {
 	ICPU(INTEL_FAM6_XEON_PHI_KNM,		knl_funcs),
 	ICPU(INTEL_FAM6_ATOM_GOLDMONT,		core_funcs),
 	ICPU(INTEL_FAM6_ATOM_GOLDMONT_PLUS,     core_funcs),
+	ICPU(INTEL_FAM6_ATOM_ELKHART_LAKE,	core_funcs),
 	ICPU(INTEL_FAM6_SKYLAKE_X,		core_funcs),
 	{}
 };

projectacrn/acrn-kernel

drivers/cpufreq/intel_pstate.c
@@ -1933,6 +1933,7 @@ static const struct x86_cpu_id intel_pstate_cpu_ids[] = {
 	ICPU(INTEL_FAM6_XEON_PHI_KNM,		knl_funcs),
 	ICPU(INTEL_FAM6_ATOM_GOLDMONT,		core_funcs),
 	ICPU(INTEL_FAM6_ATOM_GOLDMONT_PLUS,     core_funcs),
+	ICPU(INTEL_FAM6_ATOM_TREMONT,		core_funcs),
 	ICPU(INTEL_FAM6_SKYLAKE_X,		core_funcs),
 	{}
 };